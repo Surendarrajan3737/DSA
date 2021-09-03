cd8abafa9bf476b364cf16fa619b786c30a36754
Machine_learner <48998012+Surendarrajan3737@users.noreply.github.com>
2021-09-03 21:48:05 +0530

    Create Search_a_value_in_BST.cpp

---
+++ Search_a_value_in_BST.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!=NULL){
+            if(root->val == val) return root;
+            else if(root->val < val){
+                root = root->right;
+            }
+            else root = root->left;
+        }
+        return NULL;
+    }
+};
